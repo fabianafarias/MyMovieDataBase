dbfa96925d1eef253e4a55d12a57651edf367fe1
fabiana <farias.fabianam@gmail.com>
2022-09-13 14:44:44 -0300

    organizing project

--- app/src/main/java/com/fabianafarias/mymoviedatabase/ui/MainActivity.kt
+++ app/src/main/java/com/fabianafarias/mymoviedatabase/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.fabianafarias.mymoviedatabase
+package com.fabianafarias.mymoviedatabase.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fabianafarias.mymoviedatabase.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
